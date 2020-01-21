4f3ab9e974bd8b2be157001bfe4875b10a7f3937
github username <github email address>
2020-01-20 21:02:50 -0500

    Added login controller and modified application controller to store app secret in env rile.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,22 @@
 class Api::V1::UsersController < ApplicationController
-  def create 
+  skip_before_action :authorized, only: [:create]
+
+  def profile
+    render json: { user: current_user }, status: :accepted
+  end
+
+  def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: @user }, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:username, :password, :bio, :avatar)
   end
